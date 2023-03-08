a095fd3d7324a1d0aa9e5ad9a30e7473398ab27b
Thiago Izidoro <thiagomachadox@gmail.com>
2023-03-07 22:50:28 -0300

    ch 4: changing dom

--- cypress/e2e/changing_DOM_start.cy.js
+++ cypress/e2e/changing_DOM_start.cy.js
@@ -1,15 +1,40 @@
 /// <reference types="cypress" />
+const { faker } = require("@faker-js/faker")
 
-beforeEach(() => {
+let boardId = faker.datatype.uuid()
 
-  cy
-    .visit('/');
+describe("Chaining DOM commands", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
 
-});
+  it("creates a new board", () => {
+    cy.get('[data-cy="create-board"]').click()
+    cy.get('[data-cy="new-board-input"]').type(`board ${boardId}`)
+    cy.contains("Save").should("exist").click()
+  })
 
-it('Changing the DOM', () => {
+  it("star a board", () => {
+    // star first board
+    // cy.get('[data-cy="star"]').click({ force: true })
+    cy.get('[data-cy="star"]').eq(0).invoke("show").click()
+  })
 
-  cy
-    .get('[data-cy="star"]')
+  it("mark a task as overdue", () => {
+    // open board
+    cy.contains("board 1").invoke("show").click()
 
+    // change DOM
+    cy.get('[data-cy="task"]').eq(0).invoke("addClass", "overDue")
+  })
+
+  it("trigger an event listener via dom", () => {
+    cy.get('[data-cy="board-item"]').eq(0).trigger("mouseover")
+
+    cy.get("[data-cy=star]").should("be.visible")
+
+    cy.get('[data-cy="board-item"]').eq(0).trigger("mouseout")
+
+    cy.get("[data-cy=star]").should("not.be.visible")
+  })
 })
