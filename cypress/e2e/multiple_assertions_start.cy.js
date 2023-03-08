c5af8021498a2606f8c2fcc1ff33433635942b6f
Thiago Izidoro <thiagomachadox@gmail.com>
2023-03-07 21:40:27 -0300

    ch 3: multiple assertions

--- cypress/e2e/multiple_assertions_start.cy.js
+++ cypress/e2e/multiple_assertions_start.cy.js
@@ -1,21 +1,22 @@
 /// <reference types="cypress" />
 
 beforeEach(() => {
-
-  cy
-    .visit('/board/2305140181')
+  cy.visit("/board/30157046838")
 })
 
-it('Multiple assertions', () => {
+it("Multiple assertions", () => {
+  // then does not contain retry logic
+  // cy.get("[data-cy=task]").then((item) => {
+  //   expect(item[0]).to.contain.text("task 1")
+  //   expect(item[1]).to.contain.text("task 2")
+  // })
 
-  cy
-    .get('[data-cy=task]')
-    .eq(0)
-    .should('contain.text', 'bread')
-
-  cy
-    .get('[data-cy=task]')
-    .eq(1)
-    .should('contain.text', 'milk')
-
+  // should does retry logic
+  cy.get("[data-cy=task]").should((item) => {
+    if (item.length !== 3) {
+      throw new Error("not enough elements")
+    }
+    expect(item[0]).to.contain.text("task 2")
+    expect(item[1]).to.contain.text("task 1")
+  })
 })
